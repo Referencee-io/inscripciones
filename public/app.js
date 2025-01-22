655beabe5d1adfb38430f77eefb8df459fb38e17
Refereence <hola@refereence.io>
2025-01-22 15:41:17 -0600

    Prueba para config de Firebase

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 fetch("config.json")
   .then((response) => response.json())
   .then((config) => {
-    console.log("API_KEY:", config);
+    console.log("Hay respuesta");
     // Usa la API_KEY para hacer solicitudes u otras tareas
   });
